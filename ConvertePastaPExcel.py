1880b26c56187b1f3cfc139048818a70394c6c7b
marcioottoboni <marcio_ottoboni@hotmail.com>
2023-03-23 06:29:40 -0300

    1 versao

---
+++ ConvertePastaPExcel.py
@@ -0,0 +1,33 @@
+import os
+import csv
+import openpyxl
+
+# Pedir ao usuário o caminho da pasta e a lista de cabeçalhos
+pasta = input("Digite o caminho da pasta: ")
+cabecalhos = input("Digite a lista de cabeçalhos separados por vírgulas: ").split(",")
+
+# Criar um objeto Workbook do openpyxl
+workbook = openpyxl.Workbook()
+
+# Iterar sobre todos os arquivos na pasta
+for filename in os.listdir(pasta):
+    if filename.endswith(".txt"):
+        # Abrir o arquivo texto
+        with open(os.path.join(pasta, filename), "r") as arquivo:
+            # Ler o conteúdo do arquivo e dividir em linhas
+            conteudo = arquivo.read()
+            linhas = conteudo.split("\n")
+
+            # Criar um objeto Worksheet do openpyxl com o nome do arquivo
+            worksheet = workbook.create_sheet(title=filename)
+
+            # Adicionar os cabeçalhos à primeira linha da planilha
+            worksheet.append(cabecalhos)
+
+            # Adicionar as linhas restantes à planilha
+            for linha in linhas:
+                valores = linha.split(";")
+                worksheet.append(valores)
+
+# Salvar o arquivo Excel
+workbook.save("resultado.xlsx")
