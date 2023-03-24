ad2d880eae1d42408331b40f2d9bbd6881c5760c
marcioottoboni <marcio_ottoboni@hotmail.com>
2023-03-24 12:11:41 +0000

    Com criacao de cabecalho automatico
    O arquivo  ja esta criando o cabecalho ma planilha excel   tendo como base o script  SQL  que foi utilizado para gerar os textos pelo toad

--- ConvertePastaPExcel.py
+++ ConvertePastaPExcel.py
@@ -3,9 +3,23 @@ import csv
 import openpyxl
 
 # Pedir ao usuário o caminho da pasta e a lista de cabeçalhos
-pasta = input("Digite o caminho da pasta: ")
-cabecalhos = input("Digite a lista de cabeçalhos separados por vírgulas: ").split(",")
+pasta = input("Digite o caminho da pasta com os arquivos gerados pelo toad: ")
+script_base = input("Digite o caminho e nome  da script  SQL: ")
 
+def cria_cabecalho(script_base):
+   #C:\Users\e070800\OneDrive - Mastercard\Documents\Python\Gerenciar Arquivos com Python
+    lista = []
+    with open(os.path.join(script_base), "r") as arquivo:
+        conteudo = arquivo.read()
+        linhas = conteudo.split("\n")
+        for item in linhas:
+            inicio = item.find('"') + len('"')
+            fim = item.find('"', inicio)
+            if inicio >= 0 and fim >= 0 and  item[inicio:fim]  != ';':
+                print(item[inicio:fim])
+                lista += [item[inicio:fim]]
+    return lista
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Criar um objeto Workbook do openpyxl
 workbook = openpyxl.Workbook()
 # Iterar sobre todos os arquivos na pasta
@@ -19,6 +33,7 @@ for filename in os.listdir(pasta):
             # Criar um objeto Worksheet do openpyxl com o nome
             worksheet = workbook.create_sheet(title=filename)
             # Adicionar os cabeçalhos à primeira linha da planilha
+            cabecalhos = cria_cabecalho(script_base)
             worksheet.append(cabecalhos)
             # Adicionar as linhas restantes à planilha
             for linha in linhas:
